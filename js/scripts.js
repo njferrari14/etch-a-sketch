f3d35f43ebe105d99168af37d523e71b0b48f145
njferrari14 <njferrari14@gmail.com>
2020-01-14 23:54:55 -0600

    trying to figure out how to make it draw only when the mouse is clicked down

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,39 @@
 let rows = [];
 let blocks = [];
 let container = document.getElementById('container');
+let isClicked = false;
 
+container.addEventListener('mousedown', e => {
+    isClicked = true;
+    console.log('hello?');
+});
 
 for(row = 0; row < 32; row++) {
     rows[row] = document.createElement('div');
-    rows[row].classList.add("row");
+    rows[row].classList.add('row');
     container.appendChild(rows[row]);
     for(column = 0; column < 32; column++) {
         blocks[column] = document.createElement('div');
-        blocks[column].classList.add("block");
+        blocks[column].classList.add('block');
         rows[row].appendChild(blocks[column]);
     }
 }
 
-for(i = 0; i < rows.length; i++) {
-    rows[i].addEventListener("mouseover", function(event){
-        event.target.style.backgroundColor = "black";
 
-        setTimeout(function() {
-            event.target.style.backgroundColor = "";
-        }, 10000);
-    }, false);
+if(isClicked) {
+    console.log('yup');
+    for(i = 0; i < rows.length; i++) {
+        rows[i].addEventListener('mouseover', function(draw) {
+            event.target.style.backgroundColor = 'black';
+
+            setTimeout(function() {
+                event.target.style.backgroundColor = '';
+            }, 10000);
+        }, false);
+    }
+} else {
+    console.log('nope');
 }
-
 
 
 
