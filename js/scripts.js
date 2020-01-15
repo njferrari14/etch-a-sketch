2cd7d297f3f983e952936d1bb97d45f28d41005e
njferrari14 <njferrari14@gmail.com>
2020-01-14 22:55:57 -0600

    added folders, added hover functionality

--- js/scripts.js
+++ js/scripts.js
@@ -2,16 +2,30 @@ let rows = [];
 let blocks = [];
 let container = document.getElementById('container');
 
-for(row = 0; row < 16; row++) {
+
+for(row = 0; row < 32; row++) {
     rows[row] = document.createElement('div');
     rows[row].classList.add("row");
     container.appendChild(rows[row]);
-    for(column=0; column < 16; column++) {
+    for(column = 0; column < 32; column++) {
         blocks[column] = document.createElement('div');
         blocks[column].classList.add("block");
         rows[row].appendChild(blocks[column]);
     }
 }
+
+for(i = 0; i < rows.length; i++) {
+    rows[i].addEventListener("mouseover", function(event){
+        event.target.style.backgroundColor = "black";
+
+        setTimeout(function() {
+            event.target.style.backgroundColor = "";
+        }, 10000);
+    }, false);
+}
+
+
+
 
 
 
