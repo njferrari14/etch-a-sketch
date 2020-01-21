f6f2e9dc444c255c887f1e796e9b71e0aad055ca
njferrari14 <njferrari14@gmail.com>
2020-01-21 15:35:50 -0600

    made it more fun

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,92 @@
 let rows = [];
 let blocks = [];
 let container = document.getElementById('container');
-let isClicked = false;
+let color = 'black';
+let isRandom = false;
 
-container.addEventListener('mousedown', e => {
-    isClicked = true;
-    console.log('hello?');
-});
+clearBoard();
 
-for(row = 0; row < 32; row++) {
-    rows[row] = document.createElement('div');
-    rows[row].classList.add('row');
-    container.appendChild(rows[row]);
-    for(column = 0; column < 32; column++) {
-        blocks[column] = document.createElement('div');
-        blocks[column].classList.add('block');
-        rows[row].appendChild(blocks[column]);
+document.getElementById('btn').onclick = function() {reset()};
+document.getElementById('clr').onclick = function() {pickColor()};
+document.getElementById('rnd').onclick = function() {setRandom()};
+
+function setGrid() {
+    let x = prompt("Please enter the number of rows. Must not be greater than 100.", 16);
+    if(x != null && x <= 100 && x > 0) {
+        let rowHeight = parseFloat((1.00 / x) * 100).toPrecision(6);;
+        for(row = 0; row < x; row++) {
+            rows[row] = document.createElement('div');
+            rows[row].classList.add('row');
+            container.appendChild(rows[row]);
+            rows[row].style.height = rowHeight + '%';
+            for(column = 0; column < x; column++) {
+                let block = blocks[column];
+                block = document.createElement('div');
+                block.classList.add('block');
+                rows[row].appendChild(block);
+                block.style.width = rowHeight + '%';
+            }
+        }
+    } else {
+        setGrid();
     }
 }
 
-
-if(isClicked) {
-    console.log('yup');
-    for(i = 0; i < rows.length; i++) {
-        rows[i].addEventListener('mouseover', function(draw) {
-            event.target.style.backgroundColor = 'black';
-
-            setTimeout(function() {
-                event.target.style.backgroundColor = '';
-            }, 10000);
-        }, false);
+function setListener() {
+    if(!isRandom){
+        for(i = 0; i < rows.length; i++) {
+            rows[i].addEventListener('mouseover', function(event) {
+                event.target.style.backgroundColor = color;
+            }, false);
+        }
+    } else {
+        for(i = 0; i < rows.length; i++) {
+            rows[i].addEventListener('mouseover', function(event) {
+                event.target.style.backgroundColor = getRandomColor();
+            }, false);
+        }
     }
-} else {
-    console.log('nope');
 }
 
+function setRandom() {
+    if (!isRandom) {
+        isRandom = true;
+    } else {
+        isRandom = false;
+    }
+    clearBoard();
+}
 
+function pickColor() {
+    isRandom = false;
+    color = prompt('Enter a color. Examples: "green" "blue" "#FF5733');
+    clearBoard();
+}
 
+function getRandomColor() {
+    let letters = '0123456789ABCDEF';
+    let value = '#';
+    for(var i = 0; i < 6; i++) {
+        value += letters[Math.floor(Math.random() * 16)];
+    }
+    return value;
+}
 
+function clearBoard() {
+    let allBlocks = document.getElementsByClassName('block');
+    let allRows = document.getElementsByClassName('row');
+    while(allBlocks.length > 0) {
+        allBlocks[0].parentNode.removeChild(allBlocks[0]);
+    }
+    while(allRows.length > 0) {
+        allRows[0].parentNode.removeChild(allRows[0]);
+    }
+    setGrid();
+    setListener();
+}
 
-
+function reset() {
+    color = 'black';
+    isRandom = false;
+    clearBoard();
+}
